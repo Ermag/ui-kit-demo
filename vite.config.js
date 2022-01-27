9d324a8c78a3c7c66b295039eec12b72bf9f264a
Ermag <ermag.bg@gmail.com>
2022-01-27 02:11:48 +0200

    Build config.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
+
+import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/stories/index.js'),
+      name: 'UI-Kit',
+      fileName: (format) => `ui-kit.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // Add external deps here
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+  plugins: [vue()],
+});
